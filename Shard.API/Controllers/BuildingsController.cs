b3dd8b8629329e9e036c636dbd38ca4ee5fb023d
mrredcoding <cedric.alonso.pro@gmail.com>
2025-06-05 17:48:20 +0200

    Version automation for swagger hub + documentation

--- Shard.API/Controllers/BuildingsController.cs
+++ Shard.API/Controllers/BuildingsController.cs
@@ -22,6 +22,15 @@ public class BuildingsController : ControllerBase
         _logger = logger;
     }
     
+    /// <summary>
+    /// Creates a new building for the given user.
+    /// </summary>
+    /// <param name="userId">Id of the user for whom to create the building.</param>
+    /// <param name="building">Details of the building to create.</param>
+    /// <returns>The created building.</returns>
+    /// <response code="201">If the building was successfully created.</response>
+    /// <response code="400">If the request body is invalid.</response>
+    /// <response code="404">If the user was not found.</response>
     [HttpPost]
     [ProducesResponseType(typeof(BuildingContract), 201)]
     [ProducesResponseType(400)]
@@ -48,6 +57,14 @@ public class BuildingsController : ControllerBase
         }
     }
 
+    // <summary>
+    /// Gets all buildings for a specific user.
+    /// </summary>
+    /// <param name="userId">Id of the user whose buildings are requested.</param>
+    /// <returns>A list of buildings.</returns>
+    /// <response code="200">Returns the list of buildings.</response>
+    /// <response code="400">If the user id is invalid.</response>
+    /// <response code="404">If the user was not found.</response>
     [HttpGet]
     [ProducesResponseType(typeof(List<BuildingContract>), 200)]
     [ProducesResponseType(400)]
@@ -75,6 +92,15 @@ public class BuildingsController : ControllerBase
         }
     }
 
+    /// <summary>
+    /// Gets a specific building for a user.
+    /// </summary>
+    /// <param name="userId">Id of the user.</param>
+    /// <param name="buildingId">Id of the building.</param>
+    /// <returns>The building.</returns>
+    /// <response code="200">Returns the requested building.</response>
+    /// <response code="400">If any parameter is invalid.</response>
+    /// <response code="404">If the user or building was not found.</response>
     [HttpGet("{buildingId}")]
     [ProducesResponseType(typeof(BuildingContract), 200)]
     [ProducesResponseType(400)]
@@ -100,6 +126,16 @@ public class BuildingsController : ControllerBase
         }
     }
 
+    /// <summary>
+    /// Builds a unit using a starport.
+    /// </summary>
+    /// <param name="userId">Id of the user.</param>
+    /// <param name="starPortId">Id of the starport building used for construction.</param>
+    /// <param name="unit">The unit to build.</param>
+    /// <returns>The built unit.</returns>
+    /// <response code="200">Returns the built unit.</response>
+    /// <response code="400">If input is invalid.</response>
+    /// <response code="404">If the user or starport was not found.</response>
     [HttpPost("{starPortId}/queue")]
     [ProducesResponseType(typeof(UnitContract), 200)]
     [ProducesResponseType(400)]
